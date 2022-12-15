e8142d37023e7952d3724cf5664493fa1d13a2c3
Mounica Kadapagunta <88759412+mounicakadapagunta@users.noreply.github.com>
2022-12-15 18:08:39 +0000

    listfiles created

---
+++ bashscripts/listfiles.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+echo "enter directory name"
+read dir
+if [ -d $dir ]
+then
+echo "list of files in the directory"
+ls –l $dir|egrep ‘^d’
+else
+echo "enter proper directory name"
+fi
+
+
+
+//code need to be edited  further more 
